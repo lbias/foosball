f3711ec152c0fbeec2adbae08d46f9acf061a80e
lbias <306508354@qq.com>
2017-07-31 17:41:54 +0800

    add players to teams of match, test

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,18 @@
 class Match < ApplicationRecord
   has_many :teams, dependent: :destroy
-  has_many :games, dependent: :destroy  
+  has_many :games, dependent: :destroy
+
+  def add(grouped_players)
+    #todo: add options for grouping players
+    teams.each_with_index do |team, index|
+      team.player_ids = grouped_players[index].map(&:id)
+    end
+  end
+
+  def add_players_by(player_ids)
+    if player_ids.size == 2
+      grouped_players = player_ids.map { |_, ids| User.find(ids) }
+      add(grouped_players)
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Match, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:match) { build(:match) }
+
+  describe '#add' do
+    let(:players) { (1..4).map { create(:user) } }
+
+    it { expect(subject).to respond_to(:add) }
+    it 'add players to teams of match' do
+      match.add(players.in_groups(2))
+      expect(match.players).to match_array(players)
+    end
+  end
 end
