d3cbcb855bdffd66d43da2f977dc75d68cbe314a
lbias <306508354@qq.com>
2017-07-31 14:21:38 +0800

    model team colors, name, validation players_limit

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,20 @@
 class Team < ApplicationRecord
   has_many :team_users
-  has_many :players, through: :team_users, class_name: 'User', source: :user  
+  has_many :players, through: :team_users, class_name: 'User', source: :user
+
+  COLORS = %w(red blue)
+
+  validate :players_limit
+
+  def name
+    players.map(&:name).join(' & ')
+  end
+
+  private
+
+  def players_limit
+    if players.size > 2
+      errors.add(:players, "can't has more then 2 players")
+    end
+  end
 end
