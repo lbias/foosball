efd424f4acc60e5504abc87e5204b88e30d6aae2
lbias <306508354@qq.com>
2017-07-31 16:55:00 +0800

    game score validations and test

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,14 @@
 class Game < ApplicationRecord
   belongs_to :match
-  belongs_to :winner, class_name: 'Team'  
+  belongs_to :winner, class_name: 'Team'
+
+  validates_numericality_of :score1, :score2, less_than_or_equal_to: 10
+
+  def score
+    [score1, score2].join(':')
+  end
+
+  def score=(score)
+    self.score1, self.score2 = score.split(':')
+  end    
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { expect(subject).to respond_to(:match) }
+
+  describe 'validations' do
+
+    it 'is invalid with score more then 10' do
+      expect(build(:game, score1: 11)).to_not be_valid
+    end
+
+    it 'is invalid with equal scores' do
+      game = build(:game, score1: 5, score2: 5)
+      game.valid?
+      expect(game.errors[:scores].any?).to be true
+    end
+  end
+
+  it '#score shows common score' do
+    game = build(:game, score1: 3, score2: 10)
+    expect(game.score).to eq('3:10')
+  end
+
+  it '#score= set scores' do
+    game = build(:game, score: '3:6')
+    expect([game.score1, game.score2]).to eq [3, 6]
+  end
 end
